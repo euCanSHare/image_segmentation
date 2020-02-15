5c9dc4112ffcd0b676dee0b933accd7a6bbacb1a
Victor M. Campello <vicmancr@gmail.com>
2020-02-16 00:54:43 +0100

    Cleaning unnecesary code for segmentation tool.

--- eval.py
+++ eval.py
@@ -1,77 +1,27 @@
 import os
-import sys
 import glob
 import logging
-import argparse
 from importlib.machinery import SourceFileLoader
 
 import config.system as sys_config
-from utils import utils_gen
 from model import Model
 from evaluate_patients import score_data as evaluation
 
 
+def run(model_name, datasets):
 
-wd = os.path.dirname(os.path.realpath(__file__))
-
-parser = argparse.ArgumentParser(
-    description="Script to evaluate a neural network model"
-)
-parser.add_argument("MODEL_NAME", type=str, help="Name of experiment to use")
-parser.add_argument('-i', '--iter', type=int, help='which iteration to use')
-parser.add_argument('-d', '--dataset', type=str, help='Select which dataset to evaluate.')
-
-
-if __name__ == "__main__":
-    args = parser.parse_args()
-
-    dataset = args.dataset
-    assert dataset is not None, 'Please, provide dataset to segment.'
-    all_datasets = [f for f in next(os.walk(sys_config.data_base))[1] if f[:2] != '__']
-    if '-' in dataset:
-        datasets = dataset.split('-')
-        logging.info('Evaluating more than one dataset at the same time: {0}'.format(datasets))
-        for ds in datasets:
-            assert ds in all_datasets, 'Dataset "{0}" not available. \
-                Please, choose one of the following: {1}'.format(ds, all_datasets)
-    else:
-        assert dataset in all_datasets, 'Dataset "{0}" not available. \
-            Please, choose one of the following: {1}'.format(dataset, all_datasets)
-
-
-    use_iter = args.iter
-    if use_iter:
-        logging.info('Using iteration: {0}'.format(use_iter))
-
-    model_path = os.path.join(sys_config.log_root, args.MODEL_NAME)
+    model_path = os.path.join(sys_config.log_root, model_name)
     config_file = glob.glob(model_path + '/*py')[0]
     config_module = config_file.split('/')[-1].rstrip('.py')
     exp_config = SourceFileLoader(config_module, os.path.join(config_file)).load_module()
 
-    output_path = os.path.join(model_path, dataset, 'predictions')
+    output_path = os.path.dirname(datasets[0])
     logging.warning('Saving segmentations on {}'.format(output_path))
 
     model = Model(exp_config)
-    database = dataset if '-' not in dataset else datasets
 
-    for outp, subset in zip([output_path], ['training', 'testing']):
-        path_pred = os.path.join(outp, 'prediction')
-        path_image = os.path.join(outp, 'image')
-        utils_gen.makefolder(path_pred)
-        utils_gen.makefolder(path_image)
+    init_iteration = evaluation(model, output_path, model_path,
+                                datasets, exp_config=exp_config,
+                                do_postprocessing=True)
 
-        path_gt = os.path.join(outp, 'ground_truth')
-        path_diff = os.path.join(outp, 'difference')
-        path_eval = os.path.join(outp, 'eval')
-        utils_gen.makefolder(path_diff)
-        utils_gen.makefolder(path_gt)
-
-        init_iteration = evaluation(model,
-                                    outp,
-                                    model_path,
-                                    database,
-                                    subset,
-                                    exp_config=exp_config,
-                                    do_postprocessing=True,
-                                    evaluate_all=True,
-                                    use_iter=use_iter)
+    return init_iteration
--- evaluate_patients.py
+++ evaluate_patients.py
@@ -37,8 +37,7 @@ def crop_or_pad_volume_to_size(vol, nx, ny):
     return vol_cropped
 
 
-def score_data(model, output_folder, model_path, dataset, subset, exp_config, 
-               do_postprocessing=False, evaluate_all=False, use_iter=None):
+def score_data(model, output_folder, model_path, datasets, exp_config, do_postprocessing=False):
 
     batch_size = 1
     num_channels = exp_config.nlabels
@@ -57,11 +56,7 @@ def score_data(model, output_folder, model_path, dataset, subset, exp_config,
     with tf.Session() as sess:
 
         sess.run(init)
-        if not use_iter:
-            checkpoint_path = utils_gen.get_latest_model_checkpoint_path(model_path, 'model_best_dice.ckpt')
-        else:
-            checkpoint_path = os.path.join(model_path, 'model_best_dice.ckpt-%d' % use_iter)
-
+        checkpoint_path = utils_gen.get_latest_model_checkpoint_path(model_path, 'model_best_dice.ckpt')
         saver.restore(sess, checkpoint_path)
 
         init_iteration = int(checkpoint_path.split('/')[-1].split('-')[-1])
@@ -70,89 +65,57 @@ def score_data(model, output_folder, model_path, dataset, subset, exp_config,
         total_volumes = 0
 
         # Select image pixel size
-        if exp_config.model_type == 'rnn':
-            nx, ny = exp_config.image_size[1:]
-        elif exp_config.model_type == 'multimodal':
-            nx, ny = exp_config.image_size[1:]
-        else:
-            nx, ny = exp_config.image_size[:2]
-
-        data = Dataset(dataset, subset, exp_config.data_mode, (nx, ny), exp_config.target_resolution)
-        gt_exists = False
+        nx, ny = exp_config.image_size[:2]
+        data = Dataset(datasets, exp_config.data_mode, (nx, ny), exp_config.target_resolution)
 
         # Iterate over volumes and slices of dataset
         for idx, volume in enumerate(data):
-            gt_exists = volume.mask_exist
             predictions = []
 
-            # Test only in images out of training set by cross validation
-            train_test = 'test'
-
-            if train_test == 'train':
-                continue
-
             logging.info(' ----- Doing image: -------------------------')
-            logging.info('  Doing: {}'.format(volume.filepath))
+            logging.info(' {}'.format(volume.filepath))
             logging.info(' --------------------------------------------')
 
-            mask_dat, img_dat = [], []
+            img_dat = []
             start_time = time.time()
 
-            if exp_config.data_mode == '2D':
+            for i, slc in enumerate(volume):
+                if i == 0:
+                    img_dat = volume.img_dat
 
-                for i, slc in enumerate(volume):
-                    if i == 0:
-                        img_dat = volume.img_dat
-                        if gt_exists:
-                            mask_dat = volume.mask_dat
+                x, y = slc.shape
+                slice_cropped = slc.img_cropped
+                x_s, y_s, x_c, y_c = slc.cropped_boundaries
 
-                    x, y = slc.shape
-                    slice_cropped = slc.img_cropped
-                    x_s, y_s, x_c, y_c = slc.cropped_boundaries
-                    if gt_exists:
-                        mask = slc.mask
+                # GET PREDICTION
+                network_input = np.float32(np.tile(np.reshape(slice_cropped, (nx, ny, 1)), (batch_size, 1, 1, 1)))
+                _, logits_out = sess.run([mask_pl, softmax_pl], feed_dict={images_pl: network_input})
+                prediction_cropped = np.squeeze(logits_out[0,...])
 
-                    # GET PREDICTION
-                    network_input = np.float32(np.tile(np.reshape(slice_cropped, (nx, ny, 1)), (batch_size, 1, 1, 1)))
-                    mask_out, logits_out = sess.run([mask_pl, softmax_pl], feed_dict={images_pl: network_input})
-                    prediction_cropped = np.squeeze(logits_out[0,...])
-
-                    # ASSEMBLE BACK THE SLICES
-                    slice_predictions = np.zeros((x,y,num_channels))
-                    # insert cropped region into original image again
-                    if x > nx and y > ny:
-                        slice_predictions[x_s:x_s+nx, y_s:y_s+ny,:] = prediction_cropped
+                # ASSEMBLE BACK THE SLICES
+                slice_predictions = np.zeros((x,y,num_channels))
+                # insert cropped region into original image again
+                if x > nx and y > ny:
+                    slice_predictions[x_s:x_s+nx, y_s:y_s+ny,:] = prediction_cropped
+                else:
+                    if x <= nx and y > ny:
+                        slice_predictions[:, y_s:y_s+ny,:] = prediction_cropped[x_c:x_c+ x, :,:]
+                    elif x > nx and y <= ny:
+                        slice_predictions[x_s:x_s + nx, :,:] = prediction_cropped[:, y_c:y_c + y,:]
                     else:
-                        if x <= nx and y > ny:
-                            slice_predictions[:, y_s:y_s+ny,:] = prediction_cropped[x_c:x_c+ x, :,:]
-                        elif x > nx and y <= ny:
-                            slice_predictions[x_s:x_s + nx, :,:] = prediction_cropped[:, y_c:y_c + y,:]
-                        else:
-                            slice_predictions[:, :,:] = prediction_cropped[x_c:x_c+ x, y_c:y_c + y,:]
+                        slice_predictions[:, :,:] = prediction_cropped[x_c:x_c+ x, y_c:y_c + y,:]
 
-                    # RESCALING ON THE LOGITS
-                    if gt_exists:
-                        prediction = transform.resize(slice_predictions,
-                                                      (mask.shape[0], mask.shape[1], num_channels),
-                                                      order=1,
-                                                      preserve_range=True,
-                                                      mode='constant')
-                    else:  # This can occasionally lead to wrong volume size, therefore if gt_exists
-                           # we use the gt mask size for resizing.
-                        prediction = transform.resize(slice_predictions,
-                                                      (img_dat[0].shape[0], img_dat[0].shape[1], num_channels),
-                                                      order=1,
-                                                      preserve_range=True,
-                                                      mode='constant')
+                # RESCALING ON THE LOGITS
+                prediction = transform.resize(slice_predictions,
+                                              (img_dat[0].shape[0], img_dat[0].shape[1], num_channels),
+                                              order=1,
+                                              preserve_range=True,
+                                              mode='constant')
 
+                prediction = np.uint8(np.argmax(prediction, axis=-1))
+                predictions.append(prediction)
 
-                    prediction = np.uint8(np.argmax(prediction, axis=-1))
-                    predictions.append(prediction)
-
-                prediction_arr = np.transpose(np.asarray(predictions, dtype=np.uint8), (1,2,0))
-
-            elif exp_config.data_mode == '3D':
-                raise Exception('3D mode not implemented yet.')
+            prediction_arr = np.transpose(np.asarray(predictions, dtype=np.uint8), (1,2,0))
 
             # This is the same for 2D and 3D again
             if do_postprocessing:
@@ -165,40 +128,12 @@ def score_data(model, output_folder, model_path, dataset, subset, exp_config,
             logging.info('Evaluation of volume took {0} secs.'.format(elapsed_time))
 
             # Save prediced mask
-            out_file_name = os.path.join(output_folder, 'prediction',
-                                         'patient' + volume.patient_id + volume.frame_suffix + '.nii.gz')
-            if gt_exists:
-                out_affine = mask_dat[1]
-                out_header = mask_dat[2]
-            else:
-                out_affine = img_dat[1]
-                out_header = img_dat[2]
+            out_file_name = volume.filepath.rstrip('.nii.gz') + '_mask.nii.gz'
+            out_affine = img_dat[1]
+            out_header = img_dat[2]
 
             logging.info('saving to: {}'.format(out_file_name))
             utils_nii.save_nii(out_file_name, prediction_arr, out_affine, out_header)
-
-            # Save image data to the same folder for convenience
-            image_file_name = os.path.join(output_folder, 'image',
-                                           'patient' + volume.patient_id + volume.frame_suffix + '.nii.gz')
-            logging.info('saving to: {}'.format(image_file_name))
-            utils_nii.save_nii(image_file_name, img_dat[0], out_affine, out_header)
-
-            if gt_exists:
-
-                # Save GT image
-                gt_file_name = os.path.join(output_folder, 'ground_truth', 'patient' + volume.patient_id + volume.frame_suffix + '.nii.gz')
-                logging.info('saving to: {}'.format(gt_file_name))
-                utils_nii.save_nii(gt_file_name, mask_dat[0], out_affine, out_header)
-
-                # Save difference mask between predictions and ground truth
-                difference_mask = np.where(np.abs(prediction_arr - mask_dat[0]) > 0, [1], [0])
-                difference_mask = np.asarray(difference_mask, dtype=np.uint8)
-                diff_file_name = os.path.join(output_folder,
-                                                'difference',
-                                                'patient' + volume.patient_id + volume.frame_suffix + '.nii.gz')
-                logging.info('saving to: {}'.format(diff_file_name))
-                utils_nii.save_nii(diff_file_name, difference_mask, out_affine, out_header)
-
 
         logging.info('Average time per volume: {}'.format(total_time/total_volumes))
 
